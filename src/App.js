d7ee2658f0eb5e486afc79d4b13db1d122ce07b7
José Luis Orihuela Conde <orihuelaconde@gmail.com>
2023-11-27 16:50:56 -0300

    * Completando y eliminando TODOs

--- src/App.js
+++ src/App.js
@@ -20,9 +20,29 @@ function App() {
 
   const searchedTodos = todos.filter(
     (todo) => {
-      return todo.text.toLowerCase().includes(searchValue.toLowerCase());
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
     }
   );
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <React.Fragment>
@@ -39,6 +59,8 @@ function App() {
           key={todo.text} 
           text={todo.text}
           completed={todo.completed}
+          onComplete={() => completeTodo(todo.text)}
+          onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
