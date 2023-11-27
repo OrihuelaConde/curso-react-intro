7d229a79e5fd7a81497b8a21fe0f4bbd014ecdac
José Luis Orihuela Conde <orihuelaconde@gmail.com>
2023-11-27 13:53:50 -0300

    * ¿Qué es el estado?

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+const [searchValue, setSearchValue] = React.useState('');
+
     return (
         <input 
         placeholder="Hacer las compras" 
-        className="TodoSearch" 
+        className="TodoSearch"
+        value={searchValue} 
         onChange={(event) => {
-            console.log('Escribiste en el TodoSearch');
-            console.log(event);
-            console.log(event.target);
-            console.log(event.target.value);
+            setSearchValue(event.target.value);
         }}/>
     );
 }
